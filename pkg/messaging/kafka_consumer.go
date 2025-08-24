62a5319fe8bfa78a89f525ba93bf6abc01d4a6ec
sayanseksenbaev <sayan123serv@gmail.com>
2025-08-24 18:41:11 +0500

    Removing debugging

--- pkg/messaging/kafka_consumer.go
+++ pkg/messaging/kafka_consumer.go
@@ -66,8 +66,6 @@ func (c *KafkaConsumer) Start() {
 
 	for {
 		msg, err := c.consumer.ReadMessage(1000 * time.Millisecond)
-		fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-		fmt.Printf("Message :%s \n", msg)
 		if err == nil {
 			c.log.Infof("Received message: %s", string(msg.Value))
 			c.handleMessage(msg)
